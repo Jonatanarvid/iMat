0f4acddb3f6b11654a130c8a5d692ace959d93da
Evelina Frännhag <evelinafrannhag@gmail.com>
2024-05-17 11:14:02 +0200

    Fix ShopView

--- src/imat/ShopView.java
+++ src/imat/ShopView.java
@@ -10,28 +10,24 @@ import java.util.List;
 public class ShopView {
     @FXML private GridPane productGrid;
 
-
-
-
     private void clearProductGrid() {
         productGrid.getChildren().clear(); // Remove all children
         productGrid.getRowConstraints().clear(); // Optional: Clear row constraints
         //gridPane.getColumnConstraints().clear();  Optional: Clear column constraints
     }
 
-    private void populateProductGrid(List<Product> products) {
+    private void populateProductGrid(List<ProductCard> productCards) {
         int counter = 0;
-        int productAmount = products.size();
-        int rowCount = (int) (products.size() /3 + 0.5);
+        int productAmount = productCards.size();
+        int rowCount = (int) (productCards.size() /3 + 0.5);
 
         clearProductGrid();
         for (int i = 0; i< (productAmount); i++) {
-            Product product = products.get(i);
+            ProductCard productCard = productCards.get(i);
             int currentRow = i / 3;
             int currentColumn = i % 3;
 
             productGrid.add(productCard, currentRow, currentColumn);
-
         }
     }
 }
