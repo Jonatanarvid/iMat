8a533307b24fe8f6200b2545ee74ca93534db79d
Jonatanarvid <jonatan2004@hotmail.se>
2024-05-22 00:38:28 +0200

    Implemented SortOrder button with the sorting methods

--- src/imat/ShopView.java
+++ src/imat/ShopView.java
@@ -2,16 +2,19 @@ package imat;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.GridPane;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.transform.Scale;
 
 import java.io.IOException;
 import java.util.List;
 
 public class ShopView extends VBox implements ProductCardObserver {
     @FXML private GridPane productGrid;
+    private BackendController controller;
+
+    @FXML
+    private ComboBox<String> sortComboBox;
 
     public ShopView() {
         FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("product_grid.fxml"));
@@ -23,28 +26,63 @@ public class ShopView extends VBox implements ProductCardObserver {
         } catch (IOException var5) {
             throw new RuntimeException(var5);
         }
+    }
 
+    public void setBackendController(BackendController controller) {
+        this.controller = controller;
     }
 
     private void clearProductGrid() {
         productGrid.getChildren().clear(); // Remove all children
         productGrid.getRowConstraints().clear(); // Optional: Clear row constraints
-        //gridPane.getColumnConstraints().clear();  Optional: Clear column constraints
     }
 
     private void populateProductGrid(List<ProductCard> productCards) {
         int counter = 0;
         int productAmount = productCards.size();
         System.out.println(productAmount);
-        int rowCount = (int) (productCards.size() /3 + 0.5);
+        int rowCount = (int) (productCards.size() / 3 + 0.5);
 
         clearProductGrid();
-        for (int i = 0; i< (productAmount); i++) {
+        for (int i = 0; i < (productAmount); i++) {
             ProductCard productCard = productCards.get(i);
             int currentRow = i / 3;
             int currentColumn = i % 3;
 
             productGrid.add(productCard, currentColumn, currentRow);
+        }
+    }
+
+    @FXML
+    private void handleSortOrderChange() {
+        String selectedSort = sortComboBox.getValue();
+        SortOrder sortOrder;
+
+        switch (selectedSort) {
+            case "Alphabetical":
+                sortOrder = SortOrder.ALPHA;
+                break;
+            case "Reverse Alphabetical":
+                sortOrder = SortOrder.REVERSEALPHA;
+                break;
+            case "Price: Low to High":
+                sortOrder = SortOrder.PRICELOWHIGH;
+                break;
+            case "Price: High to Low":
+                sortOrder = SortOrder.PRICEHIGHLOW;
+                break;
+            default:
+                sortOrder = SortOrder.ALPHA;
+                break;
+        }
+        controller.setSortOrder(sortOrder);
+
+
+
+        if (controller != null) {
+            controller.newSearch(new Search("", sortOrder));
+        } else {
+            System.err.println("Controller is not initialized.");
         }
     }
 
