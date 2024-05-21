b05143b4caa204b8c97fc5d3980a280fb69f48eb
Jonatanarvid <jonatan2004@hotmail.se>
2024-05-22 01:47:59 +0200

    Fixing sortbutton even more, defaultSort is now determined by the first search done in the BackendController

--- src/imat/BackendController.java
+++ src/imat/BackendController.java
@@ -29,8 +29,7 @@ public class BackendController implements ProductCardObservable, FavouriteObserv
 
     public void start(ProductCardObserver productCardObserver) {
         for(Product product : dataHandler.getProducts()) {
-            ProductCard productCard = new ProductCard(product, dataHandler.getFXImage(product));
-            ShoppingItem shoppingItem = new ShoppingItem(product, 0);
+            ProductCard productCard = new ProductCard(product, dataHandler.getFXImage(product));ShoppingItem shoppingItem = new ShoppingItem(product, 0);
             productCard.addFavouriteObserver(this);
             productCard.addShoppingItemObserver(this);
             productCards.put(product, productCard);
