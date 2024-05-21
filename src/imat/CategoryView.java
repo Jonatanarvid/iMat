04004de4f79bd9936c1e70e06c242306e0eb8349
Jonatanarvid <jonatan2004@hotmail.se>
2024-05-21 16:56:45 +0200

    Created CategoryView treeview

--- src/imat/CategoryView.java
+++ src/imat/CategoryView.java
@@ -1,13 +1,20 @@
 package imat;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.scene.layout.VBox;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
+import se.chalmers.cse.dat216.project.ProductCategory;
 
 import java.io.IOException;
+import java.util.List;
 
 public class CategoryView extends VBox {
     private IMatDataHandler dataHandler;
+    @FXML
+    private TreeView<String> categoryTreeView;
 
     public CategoryView(IMatDataHandler dataHandler) {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("category_view.fxml"));
@@ -22,5 +29,36 @@ public class CategoryView extends VBox {
         }
 
         this.dataHandler = dataHandler;
+
+        TreeItem<String> rootItem = new TreeItem<>("Kategorier");
+        rootItem.setExpanded(true);
+
+        // Iterate over Categories enum to create categories and subcategories
+        for (Categories category : Categories.values()) {
+            TreeItem<String> categoryItem = new TreeItem<>(category.name());
+            List<ProductCategory> subcategories = category.convertToListOfProductCategory();
+            for (ProductCategory subcategory : subcategories) {
+                TreeItem<String> subcategoryItem = new TreeItem<>(subcategory.name());
+                categoryItem.getChildren().add(subcategoryItem);
+            }
+            rootItem.getChildren().add(categoryItem);
+        }
+
+        // Set the root item to the TreeView
+        categoryTreeView.setRoot(rootItem);
+
+        // Hide the root item
+        categoryTreeView.setShowRoot(false);
+
+        // Handle selections
+        categoryTreeView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                TreeItem<String> selectedItem = newValue;
+                System.out.println("Selected item: " + selectedItem.getValue());
+                // Handle the selection (category or subcategory)
+                // Add your custom logic here
+            }
+        });
+
     }
 }
